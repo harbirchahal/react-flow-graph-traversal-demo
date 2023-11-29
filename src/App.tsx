a661eb9dab47b97f56978529464f9e03f72ebb90
Harbir Chahal <harbirnoop@gmail.com>
2023-11-29 22:05:52 +0530

    Update appbar

--- src/App.tsx
+++ src/App.tsx
@@ -6,13 +6,40 @@ import GraphData from "./graph/GraphData";
 import GraphFlow from "./graph/GraphFlow";
 import viewSlice from "./store/viewSlice";
 
+const externalLinks = [
+  {
+    label: "View on GitHub",
+    href: "https://github.com/harbirchahal/react-flow-graph-traversal-demo",
+  },
+  {
+    label: "Edit on StackBlitz",
+    href: "https://stackblitz.com/edit/react-flow-graph-traversal-demo",
+  },
+];
+
 function App() {
   const dispatch = useDispatch();
   const activeTab = useSelector(viewSlice.select.getActiveTab);
 
   return (
     <>
-      <Toolbar start="React Flow Graph Traversal Demo" className="font-bold" />
+      <Toolbar
+        start={<div className="font-bold">React Flow Graph Traversal Demo</div>}
+        end={
+          <div className="flex gap-1">
+            {externalLinks.map((link) => (
+              <a
+                href={link.href}
+                target="_blank"
+                className="p-button p-button-secondary p-button-outlined p-button-sm px-2 no-underline"
+                style={{ height: "30px" }}
+              >
+                {link.label}
+              </a>
+            ))}
+          </div>
+        }
+      />
       <TabView
         activeIndex={activeTab}
         onTabChange={(e) => dispatch(viewSlice.actions.setActiveTab(e.index))}
